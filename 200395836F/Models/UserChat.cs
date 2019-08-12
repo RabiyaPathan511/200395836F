f167a2a1b39dea2d7319cc0a987da183e5bda016
Rabiya Pathan <200395836@student.georgianc.on.ca>
2019-08-12 17:55:14 -0400

    created userchat Model

---
+++ 200395836F/Models/UserChat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _200395836F.Models
+{
+    public class UserChat
+    {
+        //primary key user id
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }  //name of the user
+        [Required]
+        public string Message { get; set; }//message of the user
+        [Required]
+        public DateTime CreatedAt { get; set; }//date and time of created message
+    }
+}
