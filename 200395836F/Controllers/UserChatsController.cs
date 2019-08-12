ea02e3f0150329cf593501c4cdf6c90df766f001
Rabiya Pathan <200395836@student.georgianc.on.ca>
2019-08-12 18:03:32 -0400

    Deleted details edit and delete from the userchat view

--- 200395836F/Controllers/UserChatsController.cs
+++ 200395836F/Controllers/UserChatsController.cs
@@ -17,23 +17,10 @@ namespace _200395836F.Controllers
         // GET: UserChats
         public ActionResult Index()
         {
-            return View(db.UserChats.ToList());
+            return View();
         }
 
-        // GET: UserChats/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            UserChat userChat = db.UserChats.Find(id);
-            if (userChat == null)
-            {
-                return HttpNotFound();
-            }
-            return View(userChat);
-        }
+     
 
         // GET: UserChats/Create
         public ActionResult Create()
@@ -46,7 +33,7 @@ namespace _200395836F.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Message,CreatedAt")] UserChat userChat)
+        public ActionResult Create(UserChat userChat)
         {
             if (ModelState.IsValid)
             {
@@ -57,63 +44,11 @@ namespace _200395836F.Controllers
 
             return View(userChat);
         }
+        
+       
 
-        // GET: UserChats/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            UserChat userChat = db.UserChats.Find(id);
-            if (userChat == null)
-            {
-                return HttpNotFound();
-            }
-            return View(userChat);
-        }
 
-        // POST: UserChats/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Message,CreatedAt")] UserChat userChat)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(userChat).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(userChat);
-        }
-
-        // GET: UserChats/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            UserChat userChat = db.UserChats.Find(id);
-            if (userChat == null)
-            {
-                return HttpNotFound();
-            }
-            return View(userChat);
-        }
-
-        // POST: UserChats/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            UserChat userChat = db.UserChats.Find(id);
-            db.UserChats.Remove(userChat);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+       
 
         protected override void Dispose(bool disposing)
         {
